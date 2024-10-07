3373245f6139c6b7ff30400d28418ccd2100aade
chayma <shaymabenamor30@gmail.com>
2024-10-07 15:53:05 +0100

    cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cv';
+  check=true;
+  showSecondPart = false;
+
+  constructor() {
+    // Show the second part after 4 seconds
+    setTimeout(() => {
+      this.showSecondPart = true;
+    }, 3000); // Adjust this timing based on the animation duration
+  }
+
+  @HostListener('window:scroll', ['$event']) 
+  onScroll(event: any) {
+      // Your logic here
+      console.log('Window scrolled:', event);
+      this.check = false;
+  }
 }
